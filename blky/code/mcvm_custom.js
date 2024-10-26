07fa276b9de856aef44bead89f92773751544acb
1YEN-Toru <137962771+1YEN-Toru@users.noreply.github.com>
2024-10-26 11:18:11 +0900

    create

---
+++ blky/code/mcvm_custom.js
@@ -0,0 +1,7 @@
+// customizing loop blocks
+Blockly.libraryBlocks.loops.loopTypes.add('mcoc_controls_dorept');
+Blockly.libraryBlocks.loops.loopTypes.add('mcoc_controls_loop');
+
+// customizing procedure blocks
+Blockly.Blocks['procedures_ifreturn'].FUNCTION_TYPES.push('mcoc_task');
+
